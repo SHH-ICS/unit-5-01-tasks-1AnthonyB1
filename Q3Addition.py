55eeaf73ff039b72346306368bfcab0aa39d331a
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-02 14:50:24 +0000

    Almost done Q3Addition.py

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,21 +1,21 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
 import random
-while True:
-    a = (input("What is the number at the start of your series?\nenter a here:"))
-    if a.isnumeric() is False:
-        print("\nInvalid, please try again with a number:\n")
-    elif a.isnumeric() is True:
-        break
+
+x = random.randint(2,100)
+y = random.randint(2,100)
+sum = (x + y)
+print(sum)
 
 while True:
-    b = (input("What is the number at the start of your series?\nenter b here:"))
-    if b.isnumeric() is False:
-      print("\nInvalid, please try again with a number:\n")
-    elif b.isnumeric() is True:
+    print("\nThis is your math problem...")
+    print(x,"+",y,"=")
+
+    z = int(input("Your Answer:"))
+
+    if z == sum:
+        print("\nCorrect Answer!")
+
+    elif z != sum:
         break
-
-c = (int(a) + 1)
-
-x = random.randint(int(c),int(b))
-print("\nThis is a rendom number in between,", int(a), "and", int(b),":", x)
+        
