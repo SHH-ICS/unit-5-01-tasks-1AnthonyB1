f3848b85710407cd4c8a059042683e07aa1a2b77
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-03 01:13:50 +0000

    More Tweaking on Q3

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,26 +1,35 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
-import random
-flag = True
+def main():
 
+    import random
+    flag = True
 
-x = random.randint(2,100)
-y = random.randint(2,100)
-sum = (x + y)
-print(sum)
+    x = random.randint(2,100)
+    y = random.randint(2,100)
+    sum = (x + y)
+    print(sum)
 
-while flag is True:
-    print("\nThis is your math problem...")
-    print(x,"+",y,"=")
+    while flag is True:
+        print("\nThis is your math problem...")
+        print(x,"+",y,"=")
 
-    z = int(input("Your Answer:"))
+        z = int(input("Your Answer:"))
 
-    if z == sum:
-        print("\nCorrect Answer!")
-        flag = False
+        if z == sum:
+            print("\nCorrect Answer!\n")
+            flag = False
 
-    elif z == sum:
-        flag = True
-        break
-    
-    
+        elif z == sum:
+            flag = True
+            break
+        
+    r = input("Would you like another question?\nYes/No: ")
+
+    capr = str.upper(r)
+    if capr == "YES":
+        main()
+    else:
+        print("\nOk then, Have a good one!\n")
+        exit()
+main()
