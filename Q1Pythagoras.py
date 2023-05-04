91c49a3c0494bb3e892fb2c9e1d8365f344a2b83
Anthony Bengtsson <124379293+1AnthonyB1@users.noreply.github.com>
2023-05-04 11:55:46 +0000

    Project Completed

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,9 +1,21 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
 import math
-a = float(input("What is the leg length of side a?\nenter a here:"))
-b = float(input("What is the leg length of side b?\nenter b here:"))
 
-csquared = ((a**2) + (b**2))
+while True:
+    a = (input("\nWhat is the leg length of side a?\nenter a here:"))
+    if a.isnumeric() is False:
+        print("\nInvalid, please try again with a number:")
+    elif a.isnumeric() is True:
+        break
+
+while True:
+    b = (input("\nWhat is the leg length of side b?\nenter b here:"))
+    if b.isnumeric() is False:
+        print("\nInvalid, please try again with a number:")
+    elif b.isnumeric() is True:
+        break
+
+csquared = ((float(a)**2) + (float(b)**2))
 result = math.sqrt(csquared)
-print("C equals: ",result)
+print("\nC equals: ",result)
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,27 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import pygame, sys
+
+pygame.init()
+Display = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("The Sunrise of Anthony")
+
+Clock = pygame.time.Clock()
+FPS = 60
+
+while True:
+    Display.fill((100, 70, 255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # Render Stuff Here
+    pygame.draw.circle(Display, (255, 200, 0), (250,250), 150)
+    pygame.draw.line(Display, (100, 200, 100), (0, 500), (500, 500), 400)
+  
+    pygame.draw.circle(Display, (255, 255, 255), (100, 75), 30)
+    pygame.draw.circle(Display, (255, 255, 255), (60, 80), 30)
+    pygame.draw.circle(Display, (255, 255, 255), (140, 80), 30)
+
+    pygame.display.flip()
+    Clock.tick(60)
